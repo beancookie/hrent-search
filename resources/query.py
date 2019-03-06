517920181b1210d683a6d53f0b681705e0e5aeda
loveluzhong <loveluzhong@outlook.com>
2019-03-06 17:57:05 +0800

    '修改搜索策略'

--- resources/query.py
+++ resources/query.py
@@ -8,11 +8,17 @@ client = Elasticsearch('193.112.33.124:9200')
 class Query(Resource):
     def post(self):
         params = request.get_json()['params']
+        print(params)
+        if 'id' in params:
+            return Search(using=client, index='hrent').query('match', _id=params['id']).execute().to_dict()
         
         search = Search(using=client, index='hrent') \
-            .params(size=1000) \
+            .params(size=50) \
             .filter('range', price={'gt': 300}) \
-            .query('multi_match', query=params['keyword'], fields=['title', 'detail', 'address', 'traffic', 'house_type']) 
+            .filter('range', area={'gt': -1}) \
+            .filter('term', _type='ziroom') \
+            .filter('match', city=params['city']) \
+            .query('multi_match', query=params['keyword'], fields=['title', 'address', 'traffic']) 
 
         if 'price' in params:
             if int(params['price']) != -1:
@@ -22,7 +28,7 @@ class Query(Resource):
         if 'decorate' in params:
             search = search.query('match', decorate=params['decorate']) 
         
-        if 'rent_type' in params:
-            search = search.query('match', rent_type=params['rent_type']) 
+        if 'orientation' in params:
+            search = search.query('term', orientation=params['orientation']) 
         res = search.execute().to_dict()
         return res['hits']
